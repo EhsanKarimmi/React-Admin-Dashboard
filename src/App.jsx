929ea685c96ffd3c617dd33c77de50abe1b06eb7
EhsaN Karimi <ehsan.karimmi@gmail.com>
2024-05-02 04:41:01 +0330

    add: pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProductsPage from "./pages/ProductsPage";
+import UsersPage from "./pages/UsersPage";
 
 function App() {
-    return <div>App</div>;
+    return (
+        <>
+            {/* project routes */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/users" element={<UsersPage />} />
+                    <Route path="/products" element={<ProductsPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
